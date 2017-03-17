7f4d0c5ac2d9295b8fd97facdc9479a70149f93f
Jeferson Fiuza Passos <jefynho1213@hotmail.com>
2017-03-16 22:34:24 -0300

    Add files via upload

--- class/funcionario.h
+++ class/funcionario.h
@@ -1,16 +1,24 @@
 #include <iostream>
 
+using namespace std;
+
 class funcionario {
 
     private :
-        char name;
+        char* name;
         float salary;
-        char date;
+        char* date;
 
     public :
-        funcionario(char n, float s, char d){
+        funcionario(char* n, float s, char* d){
             this->name = n;
             this->salary = s;
             this->date = d;
         }
-}
+
+        void print() {
+            cout<< "Nome ="<<name<< "\nSalario= %.2f "<<salary<<"\nData="<<date << endl;
+        }
+};
+
+
---
+++ main/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../class/funcionario.h"
+
+int main(){
+    funcionario f1("Jefferson", 0.00 , "13/1/2016");
+    f1.print();
+}
